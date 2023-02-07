b30e93f62cb955fd4151f45e2a73d0db5bd4e866
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-02-07 14:13:58 -0800

    Sessions Controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+    skip_before_action :authorize, only: :create
+    
+    def create
+        employee = Employee.find_by(email: params[:email])
+        employer = Employer.find_by(email: params[:email])
+
+        if employee&.authenticate(params[:password])
+            session[:user_id] = employee.id
+            session[:is_employer] = 0
+            render json: employee, status: :created
+
+        elsif employer&.authenticate(params[:password])
+            session[:user_id] = employer.id
+            session[:is_employer] = 1
+            render json: employer, status: :created
+        else
+            render json: { errors: "Invalid Username or Password"}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+end

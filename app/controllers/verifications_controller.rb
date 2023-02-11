21841ccee260793e88403573add36d08928a6fc9
Noah King <109630641+NoahBroah@users.noreply.github.com>
2023-02-10 19:08:21 -0800

    Verification Actions

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -1,2 +1,31 @@
 class VerificationsController < ApplicationController
+    def create
+        employee = @current_user
+        verifcation = employee.verifcation.create(verifcation_params)
+        if verifcation.valid? && (session[:is_employer] === 0)
+            render json: verification, status: :created
+        else
+            render json: { errors: verifcation.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def index
+        verifcation = Verification.all
+        render json: status: :ok
+    end
+
+    def show
+        verifcation = Verification.find_by(id: params[:id])
+        render json: verifcation, status: :ok
+    end
+
+    def update
+        employer = @current_user
+        verifcation = Verification.find_by(id: params[:id])
+        if session[:is_employer] === 1 
+            render json: verifcation, status: :created
+        else
+            render json: { errors: verifcation.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
 end
